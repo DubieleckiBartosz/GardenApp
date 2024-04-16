f8be602d4977e4a939422c537aad714bf8270ab5
Ba348 <bdubielecki@gmail.com>
2024-04-16 22:42:03 +0200

    Works module - adding TagList object

---
+++ src/Modules/Works/Works.Domain/GardeningWorks/ValueObjects/TagList.cs
@@ -0,0 +1,36 @@
+﻿namespace Works.Domain.GardeningWorks.ValueObjects;
+
+internal class TagList
+{
+    private List<Tag> _tags;
+    public List<Tag> Values => _tags;
+
+    private TagList()
+    {
+        _tags = new();
+    }
+
+    public TagList(List<Tag> tags)
+    {
+        _tags = tags;
+    }
+
+    public static explicit operator TagList(string tags)
+    {
+        if (string.IsNullOrEmpty(tags))
+        {
+            return new();
+        }
+
+        List<Tag> data = tags.Split(';')
+            .Select(x => (Tag)x)
+            .ToList();
+
+        return new TagList(data);
+    }
+
+    public static implicit operator string(TagList tagList)
+    {
+        return string.Join(";", tagList._tags.Select(x => x.ToString()));
+    }
+}
